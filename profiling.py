171c8881ad38c67f2e554d8dfe19f1a2d1cf44f3
carvafel <88912806+carvafel@users.noreply.github.com>
2025-09-07 16:37:58 -0500

    Docs iniciales

---
+++ profiling.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+# 1. Cargar dataset de entrenamiento
+df = pd.read_excel("Datos_proyecto.xlsx")
+
+# 2. Generar reporte
+profile = ProfileReport(df, 
+                        title="EDA proyecto – Entrenamiento", 
+                        explorative=True)
+
+profile.to_file("eda_proyecto.html")
